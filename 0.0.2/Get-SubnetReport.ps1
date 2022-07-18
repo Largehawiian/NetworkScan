f33373bb85604349b8e89232632a7f276dcb4c54
largehawiian <jeremyorama@gmail.com>
2022-07-17 21:49:41 -0400

    Version 0.0.2

---
+++ 0.0.2/Get-SubnetReport.ps1
@@ -0,0 +1,33 @@
+#
+function Get-SubnetReport {
+    param (
+        [CmdletBinding()]
+        [switch]$ResolveDNS,
+        [Parameter(Mandatory=$True,
+            ParameterSetName = "Scan")]
+            [Parameter(ParameterSetName = "PortScan")]
+            [String]$Subnet,
+        [Parameter(Mandatory=$True,
+            Position = 0,
+            ParameterSetName = "Scan")]
+            [Parameter(ParameterSetName = "PortScan")]
+            [String]$FirstHost,
+        [Parameter(Mandatory=$True,
+            Position = 1,
+            ParameterSetName = "Scan")]
+            [Parameter(ParameterSetName = "PortScan")]
+            [String]$LastHost,
+        [Parameter(Mandatory = $True,
+            ParameterSetName = "PortScan")]
+            [string]$Port
+    )
+    if ($Port){
+        $Script:Scan = Invoke-SubnetScan -Subnet $Subnet -FirstHost $FirstHost -LastHost $LastHost | Invoke-PortScan -Port $Port 
+        [portscan]::ScanReport($Script:Scan) | Format-Table
+    }
+    if (!$Port){
+        $Script:Scan = Invoke-SubnetScan -Subnet $Subnet -FirstHost $FirstHost -LastHost $LastHost
+        [portscan]::PingReport($Script:Scan) | Sort-Object "Remote Address" -Descending |Format-Table
+    }
+    
+}
---
+++ 0.0.2/Invoke-SubnetScan.ps1
@@ -0,0 +1,35 @@
+function Invoke-SubnetScan {
+    param (
+        [switch]$ResolveDNS,
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Subnet,
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String]$FirstHost,
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String]$LastHost
+    )
+    $Script:Hosts = Foreach ($i in $FirstHost..$LastHost){
+        $Subnet.trim(".") + "." + $i
+    }
+    if (!$ResolveDNS){
+        Invoke-FastPing -HostName $Script:Hosts | Where-Object {$Null -ne $_.RoundtripAverage} 
+    }
+    if ($ResolveDNS){
+        Write-Host "DNS Server To Use?"
+        $DNSServer = Read-Host
+        $Scan = Invoke-FastPing -HostName $Script:Hosts | Where-Object {$Null -ne $_.RoundtripAverage}
+        $Results = foreach ($i in $Scan){
+            [PSCustomObject]@{
+                ComputerName = (Resolve-DNSName $i.ComputerName -Server $DNSServer -ErrorAction SilentlyContinue).NameHost
+                IPAddress = $i.ComputerName
+                RoundTripAverage = $i.RoundtripAverage
+                Online = $i.Online
+            }
+        }
+        return $Results
+    }
+    
+}
